a5962076c8665290e13fe0c98268bdcae18a512f
en30 <en30.git@gmail.com>
2022-06-25 15:40:51 +0900

    Fix tests

--- lib/yamecab.ex
+++ lib/yamecab.ex
@@ -6,21 +6,24 @@ defmodule YAMeCab do
   @on_load :load_library
 
   @doc """
-  Hello world.
+  Parse a given binary and returns nodes.
 
   ## Examples
 
-      iex> YAMeCab.hello()
-      :world
+      iex> YAMeCab.parse("すもももももももものうち")
+      [["すもも", "名詞", "一般", "*", "*", "*", "*", "すもも", "スモモ", "スモモ"], ["も", "助詞", "係助詞", "*", "*", "*", "*", "も", "モ", "モ"], ["もも", "名詞", "一般", "*", "*", "*", "*", "もも", "モモ", "モモ"], ["も", "助詞", "係助詞", "*", "*", "*", "*", "も", "モ", "モ"], ["もも", "名詞", "一般", "*", "*", "*", "*", "もも", "モモ", "モモ"], ["の", "助詞", "連体化", "*", "*", "*", "*", "の", "ノ", "ノ"], ["うち", "名詞", "非自立", "副詞可能", "*", "*", "*", "うち", "ウチ", "ウチ"], ["EOS"], [""]]
 
   """
   def parse(bin) do
+    load_library()
     port = Port.open({:spawn_driver, "yamecab"}, [:binary])
     Port.command(port, bin)
 
     receive do
       {^port, {:data, res}} ->
         res
+        |> String.split("\n")
+        |> Enum.map(&String.split(&1, ~r{[\t,]}))
     end
   end
 
--- test/yamecab_test.exs
+++ test/yamecab_test.exs
@@ -1,8 +1,4 @@
 defmodule YAMeCabTest do
   use ExUnit.Case
   doctest YAMeCab
-
-  test "greets the world" do
-    assert YAMeCab.hello() == :world
-  end
 end
